508841e5dcd1fdd1953afe49b0a52ea4c4d5fb21
LinsonAlfred <linson.a@pomelofashion.com>
2024-02-02 20:45:57 +1000

    adding tailwind and webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "css"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        // HTML LOADER
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ],
+  },
+};
